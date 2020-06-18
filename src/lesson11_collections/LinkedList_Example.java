4dc96b700a78abb02a6ee0c11bc15203dbfc6954
vs <shevtshov@gmail.com>
2020-06-18 15:39:27 +0300

    fm

---
+++ src/lesson11_collections/LinkedList_Example.java
@@ -0,0 +1,22 @@
+package lesson11_collections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedList_Example
+{
+    public static void main(String[] args) {
+
+        List<Number> numbers = new LinkedList<>();
+
+        for (int i = 1; i <= 10; i +=2) {
+            Number number = new Number(" lesson11_collections.Number:  " + i);
+            numbers.add(number);
+        }
+        for (Number number : numbers){
+            System.out.print(number);
+        }
+        numbers.remove(3);
+        System.out.println("\n" + numbers);
+    }
+}
---
+++ src/lesson11_collections/Number.java
@@ -0,0 +1,13 @@
+package lesson11_collections;
+
+public class Number {
+    String name;
+
+    public Number(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+}
