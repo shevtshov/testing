13e82a03f62b8c6208403133f5ea08ac5ce16808
vs <shevtshov@gmail.com>
2020-06-20 13:33:46 +0300

    fm

---
+++ src/HashMapExample.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap <String,String> Student = new HashMap<>();
+        Student.put("Q54336","Student1");
+        Student.put("E24453","Student2");
+        Student.put("B35354","Student3");
+        System.out.println(Student);
+
+    }
+}
